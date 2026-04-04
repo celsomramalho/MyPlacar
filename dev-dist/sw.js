da47e71ba891c7fcf01e9fdc4c31582bed5a09e7
celsomramalho <celsomramalho@gmail.com>
2026-04-04 16:10:01 -0300

    MyPlacar_claude_2.5.12c

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.fc9gnhmhlb4"
+    "revision": "0.5kk90h8r5ag"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
